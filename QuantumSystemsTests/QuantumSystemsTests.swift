3df9b05971317b14eeb95e29a0b1c2db50f03ff1
Bruce Ricketts <klmit1@me.com>
2014-12-09 10:41:07 -0500

    Initial Commit

---
+++ QuantumSystemsTests/QuantumSystemsTests.swift
@@ -0,0 +1,36 @@
+//
+//  QuantumSystemsTests.swift
+//  QuantumSystemsTests
+//
+//  Created by Bruce Ricketts on 12/9/14.
+//  Copyright (c) 2014 fireypotato. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class QuantumSystemsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
